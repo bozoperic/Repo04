217f1dc8c0ab11b82e48b2ad15f1fb6af68e5b06
BOZO <bozoperi@gmail.com>
2021-09-16 21:24:23 +0200

    more ex added

---
+++ ex06/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo) {
+    
+    //only change code bellow this line
+    var myQuotient =numOne/numTwo;
+     //only change code above this line
+    
+    return myQuotient;
+    }
+    console.log (main(5.0,2.0));   // change this
+    console.log (main(16.5,5.5));
+    console.log (main(102.0,25.5));
+    module.export=main;
---
+++ ex07/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo) {
+    
+    //only change code bellow this line
+    var remainder=numOne%numTwo;
+     //only change code above this line
+    
+    return remainder;
+    }
+    console.log (main(5,2));   // change this
+    console.log (main(15,4));
+    console.log (main(102,25));
+    module.export=main;
