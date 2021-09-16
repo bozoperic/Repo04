e07b8be895b87b963c3ce7a48d788d55a0971c27
BOZO <bozoperi@gmail.com>
2021-09-16 22:12:45 +0200

    more ex added

---
+++ ex10/src/index.js
@@ -0,0 +1,18 @@
+function main() {
+    
+    //only change code bellow this line
+    myFirstName ="Bozo";
+    myLastName ="Peric";
+    myHomeTown="Livno";
+     //only change code above this line
+    
+    return {
+
+myFirstName,
+myLastName,
+myHomeTown
+    }
+    }
+    console.log (main());   // change this
+    
+    module.export=main;
